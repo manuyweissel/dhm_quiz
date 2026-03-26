cc7111bcbbc55557b4dd067977efa3aec4a75150
manuyweissel <manuweissel@yahoo.com>
2026-03-26 12:52:39 +0100

    (vis) visual update to incorporate fav colours

--- src/data/content/palette.js
+++ src/data/content/palette.js
@@ -1,8 +1,8 @@
 export const roundPalettes = {
-  songs: ["#67b3ff", "#8fe6ff", "#ffe178"],
-  chat: ["#ff8f63", "#ffd66e", "#86ead3"],
-  list: ["#9b88ff", "#ffb870", "#ffe07d"],
-  eitherOr: ["#67d0be", "#b1f4cf", "#ffd96b"],
-  number: ["#ff7c8a", "#ffba7c", "#ffe28e"],
-  heatmap: ["#7b9dff", "#91d6ff", "#d9f577"],
+  songs: ["#67b3ff", "#8fe6ff", "#d7e8ff"],
+  heatmap: ["#e2bc5a", "#f2d98d", "#fff0c8"],
+  chat: ["#1d6a4f", "#68a985", "#d7ecdf"],
+  list: ["#8f78dd", "#c3b4ff", "#ece4ff"],
+  eitherOr: ["#ff9c63", "#ffc49a", "#ffe5d1"],
+  number: ["#ff88bb", "#ffb7d4", "#ffe1ee"],
 };
